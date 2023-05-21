bfdb13e63c434f053a9408f60bc1e670672f7721
Paul Lu <paulcoffee@gmail.com>
2023-05-21 16:52:41 +0000

    Add IPFS kubo fargate service

--- nft_lab/nft_lab_stack.py
+++ nft_lab/nft_lab_stack.py
@@ -4,11 +4,13 @@ from aws_cdk import (
     Stack,
     aws_ec2 as ec2,
     aws_ecs as ecs,
-    aws_cloud9_alpha as cloud9,
+    aws_cloud9 as cloud9,
     aws_managedblockchain as amb,
+    aws_codecommit as codecommit,
     # aws_sqs as sqs,
 )
 from constructs import Construct
+# import os
 
 
 class NftLabStack(Stack):
@@ -27,11 +29,27 @@ class NftLabStack(Stack):
             ],
             max_azs=3
         )
+        # Code commit
+        # TODO Add NFT Labs source code...
+        # _nft_repo = codecommit.Repository(self, "NFTLabCodeCommit",
+        #     repository_name="NFTLab",
+        #     code=codecommit.Code.from_directory('src')
+        # )
         
+        # ISSUE: Need a way ot share Cloud9 Environment to other
+        # Otherwise, user cannot view it on the console...
         # Create cloud9 Develope Environment
         # Cloud9-alpha will create EC2 and not showed on Cloud9 Console.
-        # TODO
-        # TODO Code commit
+        # _cloud9_env = cloud9.CfnEnvironmentEC2(
+        #     self, 'NFTLabCloud9',
+        #     instance_type='t3.large',
+        #     image_id='ubuntu-18.04-x86_64',
+        #     repositories=[cloud9.CfnEnvironmentEC2.RepositoryProperty(
+        #         path_component='/NFTLabs',
+        #         repository_url=_nft_repo.repository_clone_url_http
+        #     )],
+        # )
+        
         
         # Create ECS Cluster for IPFS node
         _ecs_cluster = ecs.Cluster(self, "IpfsEcsCluster", vpc=_vpc)
@@ -44,17 +62,19 @@ class NftLabStack(Stack):
             memory_limit_mib=2048,
             runtime_platform=ecs.RuntimePlatform(
                 operating_system_family=ecs.OperatingSystemFamily.LINUX,
-                cpu_architecture=ecs.CpuArchitecture.ARM64
+                # cpu_architecture=ecs.CpuArchitecture.ARM64
             ),
         )
         
-        # Add IPFS kubo container to task
+        # Add IPFS kubo container v0.20.0 to task
         # By default, Amazon ECS tasks that are hosted on 
         # Fargate using platform version 1.4.0 or later receive a 
         # minimum of 20 GiB of ephemeral storage
         _kubo_container = _ipfs_task.add_container(
             'IpfsKuboNode',
-            image=ecs.ContainerImage.from_registry('ipfs/kubo:latest'),
+            image=ecs.ContainerImage.from_registry(
+                'ipfs/kubo'
+            ),
             port_mappings=[
                 ecs.PortMapping(container_port=4001),
                 ecs.PortMapping(container_port=5001),
@@ -70,7 +90,48 @@ class NftLabStack(Stack):
             )
         )
         
-        # TODO create ECS service for ipfs task
+        # Secruity Group for IPFS Fargate Services
+        _ipfs_srv_sg = ec2.SecurityGroup(self, 'IpfsServiceSecurityGroup',
+                                         vpc=_vpc,
+                                         description='Allow access to IPFS nodes'
+                                         )
+
+        _ipfs_srv_sg.add_ingress_rule(
+            peer=ec2.Peer.any_ipv4(),
+            connection=ec2.Port.tcp(4001),
+            description='IPFS swarm port open to the Internet'
+        )
+
+        # _ipfs_srv_sg.add_ingress_rule(
+        #     peer=ec2.Peer.ipv4(_vpc.vpc_cidr_block),
+        #     connection=ec2.Port.tcp(9096),
+        #     description='IPFS Cluster Swarm from internal network only'
+        # )
+
+        _ipfs_srv_sg.add_ingress_rule(
+            peer=ec2.Peer.ipv4(_vpc.vpc_cidr_block),
+            connection=ec2.Port.tcp(5001),
+            description='IPFS RPC API from internal network only'
+        )
+
+        # Create ECS Fargate Service
+        _ipfs_srv = ecs.FargateService(
+            self, 'IpfsSrv',
+            cluster=_ecs_cluster,
+            task_definition=_ipfs_task,
+            assign_public_ip=True,
+            vpc_subnets=ec2.SubnetSelection(
+                availability_zones=[_vpc.availability_zones[0]],
+                one_per_az=False,
+                subnet_type=ec2.SubnetType.PUBLIC
+            ),
+            security_groups=[_ipfs_srv_sg],
+            enable_execute_command=True,
+            max_healthy_percent=100,
+            min_healthy_percent=0
+        )
+
+
         
         # Create BILLING_TOKEN for Amazon Managed Blockchain
         _billing_token = amb.CfnAccessor(self, "billing_token",
